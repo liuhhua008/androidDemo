de649e33992a6a6780857e2dcf003bc77211754d
liuhhua008 <37605173+liuhhua008@users.noreply.github.com>
2018-05-16 16:18:01 +0800

    完成jwt-token自动刷新

--- androidDemo/src/main/java/com/liu008/myapplication/MyApplication.java
+++ androidDemo/src/main/java/com/liu008/myapplication/MyApplication.java
@@ -1,6 +1,7 @@
 package com.liu008.myapplication;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.mob.MobSDK;
 
@@ -11,11 +12,17 @@ import org.xutils.x;
  */
 
 public class MyApplication extends Application {
+    private static Context mContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        mContext=getApplicationContext();
         x.Ext.init(this); //初始化xUtils
         MobSDK.init(this);//初始化短信平台
+    }
 
+    public static  Context getmContext() {
+        return mContext;
     }
 }
--- androidDemo/src/main/java/com/liu008/myapplication/interceptor/TokenInterceptor.java
+++ androidDemo/src/main/java/com/liu008/myapplication/interceptor/TokenInterceptor.java
@@ -1,15 +1,30 @@
 package com.liu008.myapplication.interceptor;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import com.alibaba.fastjson.JSON;
+import com.liu008.myapplication.MyApplication;
+import com.liu008.myapplication.R;
+import com.liu008.myapplication.model.AccessToken;
+import com.liu008.myapplication.model.UserInfo;
+import com.liu008.myapplication.model.UserManage;
+import com.liu008.myapplication.utils.MyConstant;
+import com.liu008.myapplication.utils.ResultMsg;
+import com.liu008.myapplication.utils.ResultStatusCode;
+import com.liu008.myapplication.view.LoginActivity;
 
 import java.io.IOException;
 
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
- * 自动刷新token的拦截器
+ * 自动给需要API验证的http请求加上jwt验证信息，并完成自己刷新token的拦截器
  * Created by 008 on 2018/5/15.
  */
 
@@ -17,21 +32,38 @@ public class TokenInterceptor implements Interceptor {
     private static final String TAG = "TokenInterceptor";
     @Override
     public Response intercept(Chain chain) throws IOException {
+        //统一对http请求加上jwt验证头
         Request request = chain.request();
+        SharedPreferences sp= MyApplication.getmContext().getSharedPreferences("userInfo",Context.MODE_PRIVATE);
+        request=request.newBuilder().header("Authorization", "bearer " +sp.getString("access_token",null)).build();
         Response response = chain.proceed(request);
         Log.d(TAG, "response.code=" + response.code());
         //根据和服务端的约定判断token过期
         if (isTokenExpired(response)) {
             Log.d(TAG, "自动刷新Token,然后重新请求数据");
-            //同步请求方式，获取最新的Token
-            String newToken = getNewToken();
-            //使用新的Token，创建新的请求
-            Request newRequest = chain.request()
-                    .newBuilder()
-                    .header("Authorization", "bearer " + newToken)
+            String refreshJwt=sp.getString("refresh_type",null);
+
+            //使refreshJwt去获取新的token
+            Request newRequest=chain.request().newBuilder()
+                    .url(MyConstant.APPSERVER_URL+"oauth/refreshToken")
+                    .header("Authorization", "bearer " + refreshJwt)
+                    .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"),MyConstant.AUDIENCE_CLIENTID))
                     .build();
-            //重新请求
-            return chain.proceed(newRequest);
+            //发请求刷新token请求并接收响应结果
+           Response refreshResponse=chain.proceed(newRequest);
+           ResultMsg resultMsg = JSON.parseObject(refreshResponse.body().string(), ResultMsg.class);
+           if (isTokenExpired(refreshResponse)){
+               Log.d(TAG, "你已经很久没有登录了，refresh-jwt都过期了");
+               return refreshResponse;
+           }else if (resultMsg.getErrcode()== ResultStatusCode.OK.getErrcode()){
+               //如果获取到了新token
+               AccessToken accessToken = JSON.parseObject(resultMsg.getP2pdata().toString(),AccessToken.class);
+               //保存到本地
+               UserManage.getInstance().saveUserInfo(MyApplication.getmContext(), accessToken);
+               //重新用新的jwt访问api并返回给客户端
+               return chain.proceed(request.newBuilder().header("Authorization", "bearer " +accessToken.getAccess_token()).build());
+           }
+
         }
         return response;
     }
@@ -51,16 +83,5 @@ public class TokenInterceptor implements Interceptor {
         return false;
     }
 
-    /**
-     * 同步请求方式，获取最新的Token
-     *
-     * @return api-jwt
-     */
-    private String getNewToken() throws IOException {
-        // 通过访问服务器refreshAccessToken接口，用本地的refresh-jwt获取新accessToken，同时保存到本地并且返回用来认证
-        //的API-JWT，如果refresh-jwt也过期的话。提示用户”长期未登录，重新登录"
 
-        String newToken = "";
-        return newToken;
-    }
 }
